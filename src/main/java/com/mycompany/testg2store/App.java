b26d03ac2f7c80e1186c6741b5f8a8c00b04dfe7
Luu Bao <Luu Bao@LAPTOP-7EPRO5R2>
2024-06-01 22:30:24 +0700

    Anh Bảo sửa

--- src/main/java/com/mycompany/testg2store/App.java
+++ src/main/java/com/mycompany/testg2store/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Login"), 800, 400);
+        scene = new Scene(loadFXML("Primary"), 800, 400);
         stage.setScene(scene);
         stage.setTitle("Đăng nhập");
         stage.centerOnScreen();
